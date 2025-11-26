8cb2b26aefbccc526a1a2cb54f6b52395d07d13a
Тишковський Денис <dentiskovskij@gmail.com>
2025-11-26 20:40:13 +0200

    первый коммит

---
+++ Project2/Header.h
@@ -0,0 +1,47 @@
+﻿#pragma once
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <ctime>
+using namespace std;
+
+
+
+
+
+class Шибениця {
+
+
+
+    vector<string> words = { "Apple", "House", "Water", "Table", "Cat", "RUST"
+                             "Car", "Ukraine", "PC", "Playstation", "RTX", "ChatGBT",  "PV521"};
+    string word, hidden;
+    int lives = 10;
+
+public:
+    Шибениця() {
+        srand(time(0));
+        word = words[rand() % words.size()];
+        hidden = string(word.size(), '_');
+    }
+
+    void play() {
+        while (lives > 0 && hidden != word) {
+            
+            cout << "Слово: " << hidden << "   Жизни: " << lives << "\n";
+            cout << "Буква: ";
+            char c; cin >> c;
+
+            bool ok = false;
+            for (size_t i = 0; i < word.size(); i++)
+                if (word[i] == c) hidden[i] = c, ok = true;
+
+            if (!ok) lives--;
+        }
+
+        if (hidden == word) cout << "Красавчик выиграл. Слово: " << word << "\n";
+        else cout << "Ты проиграл. Слово было: " << word << "\n";
+    }
+};
+
---
+++ Project2/Source.cpp
@@ -0,0 +1,10 @@
+﻿#include "Header.h"
+
+int main() {
+    setlocale(LC_ALL, "");
+    cout << "Добро пожаловать в игру.!\n";
+    cout << "Суть игры угодать слово.\n\n";
+    Шибениця game;
+    game.play();
+    return 0;
+}
